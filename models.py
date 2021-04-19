f519e5baf9df46e84fd1848948912ca9b38e0a00
Pemidi <rashidi.peyman@gmail.com>
2021-04-19 17:34:04 +0430

    update models

---
+++ models.py
@@ -0,0 +1,28 @@
+from peewee import MySQLDatabase, Model, TextField, CharField,BooleanField, ForeignKeyField, DateTimeField
+from config import SQL_USERNAME, SQL_PASSWORD, SQL_HOST, SQL_PORT
+from datetime import datetime
+
+
+database = MySQLDatabase('zoomit', user=SQL_USERNAME, password=SQL_PASSWORD,
+                         host=SQL_HOST, port=SQL_PORT)
+
+
+class BaseModel(Model):
+    created_time = DateTimeField(default=datetime.now())
+
+    class Meta:
+        database = database
+
+
+class Category(BaseModel):
+    name = CharField()
+
+class author(BaseModel):
+    name = CharField()
+class Article(BaseModel):
+    url = CharField(null=True)
+    title = CharField(null=True)
+    body = TextField(null=True)
+    is_completed = BooleanField(default=False)
+    category = ForeignKeyField(Category, backref='articles')
+
